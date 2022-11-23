07068ddc6de58be02445e6d7bc30639700ec44be
tamaki <94964371+tamango344@users.noreply.github.com>
2022-11-23 11:15:47 +0900

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -5,14 +5,24 @@ function start(){
     let hh = now.getHours();
     let mm = now.getMinutes();
     let ss = now.getSeconds();
+    let year = now.getYear();
+    let month = now.getMonth()+1;
+    let date = now.getDate();
+    let num= now.getDay();
+    const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let day = weekday[num];
 
     hh = hh<10? "0"+hh:hh;
     mm = mm<10? "0"+mm:mm;
     ss = ss<10? "0"+ss:ss;
+    month = month <10? "0"+month:month;
+    date = date <10? "0"+date:date;
+    
+    let today = `${year}.${month}.${date} ${day}`;
+    let time = `${hour}:${min}:${sec}`;
   
-    let result = document.getElementById("result");
-  
-    result.textContent = hh +":"+mm+":"+ss;
+    document.querySelector(".clock-date").innerText = today;
+    document.querySelector(".clock-time").innerText = time;
   }
   ,1000);
 }
