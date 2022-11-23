eb599ec188ce6c3af48881f4bbdbbee4dbab164a
tamaki <94964371+tamango344@users.noreply.github.com>
2022-11-23 11:35:54 +0900

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -2,17 +2,22 @@ function start(){
   window.setInterval(function()
   {
     let now = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let date = d.getDate();
     let hour = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
 
-    hour = hour<10? "0"+hh:hh;
-    minutes = minutes<10? "0"+mm:mm;
-    seconds = seconds<10? "0"+ss:ss;
+    month = month <10? "0"+month:month;
+    date = date <10? "0"+date:date;
+    hour = hour<10? "0"+hour:hour;
+    minutes = minutes<10? "0"+minutes:minutes;
+    seconds = seconds<10? "0"+seconds:seconds;
   
     let result = document.getElementById("result");
   
-    result.textContent = hour +":"+minutes+":"+seconds;
+    result.textContent = year+"/"month"/"+date" "+hour +":"+minutes+":"+seconds;
   }
   ,1000);
 }
